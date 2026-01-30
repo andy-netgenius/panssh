af0652dcb577d59beeb91c82e14f5862c7c129a6
Andy Inman <ainman@netgenius.co.uk>
2026-01-30 20:29:02 +0000

    File editing tests now working correctly.

--- tests/editor-test.sh
+++ tests/editor-test.sh
@@ -1,24 +1,28 @@
 #!/usr/bin/env bash
-# Fake editor for PanSSH automated tests
+
+# Fake editor for PanSSH automated tests.
 
 set -e
 
 FILE="$1"
+initial_content="Created by automated PanSSH test"
+updated_content="Edited by automated PanSSH test"
 
 if [ -z "$FILE" ]; then
     echo "editor-test.sh: No file provided" >&2
     exit 1
 fi
 
-# Show current content
+# Set initial content, or update existing content.
 if [ -f "$FILE" ]; then
-    cat "$FILE"
+    content=$(cat "$FILE")
+    # Replace initial content.
+    if [[ $content == $initial_content ]]; then
+        echo "$updated_content" > "$FILE"
+    fi
 else
-    echo "[Empty file]"
+    echo $initial_content > "$FILE"
 fi
-
-# Replace content
-echo "Edited by automated PanSSH test" >"$FILE"
 
 # Message used by the test script to detect editor exit.
 echo "editor exit" >&2
